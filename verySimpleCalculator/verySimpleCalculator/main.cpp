b5c0c4e3fcf157e94afda10298f2f2b7a2af4627
MarCodesM <marcos.murillo395@my.tccd.edu>
2020-11-25 14:21:57 -0600

    added welcome screen. and way to end program

--- verySimpleCalculator/verySimpleCalculator/main.cpp
+++ verySimpleCalculator/verySimpleCalculator/main.cpp
@@ -2,9 +2,55 @@
 #include "Input_Validation_Extended.h"
 #include <iomanip>
 #include <algorithm>
+#include <string>
 
-//***NOTE*** This program's loop is infinite, If you want to close program ctrl-C or stop it. ***NOTE***
 
+void printWelcome()
+{
+    std::cout << "=============================================" << std::endl;
+    std::cout << "You are able to do the following calculations" << std::endl;
+    std::cout << "      Add, Subtract, Multiply, Divide        " << std::endl;
+    std::cout << " If you want to end the program, type 'end'. " << std::endl;
+    std::cout << " when it prompts you to enter the operation " << std::endl;
+    std::cout << "=============================================" << std::endl;
+}
+
+bool calculateResult(double numOne, double numTwo, std::string operation)
+
+{
+    if (operation.compare("add") == 0 || operation.compare("+") == 0)
+    {
+        std::cout << "Result: " << numOne + numTwo << std::endl;
+        return true;
+    }
+    else if (operation.compare("subtract") == 0 || operation.compare("-") == 0)
+    {
+        std::cout << "Result: " << numOne - numTwo << std::endl;
+        return true;
+
+    }
+    else if (operation.compare("multiply") == 0 || operation.compare("*") == 0)
+    {
+        std::cout << "Result: " << numOne * numTwo << std::endl;
+        return true;
+
+
+    }
+    else if (operation.compare("divide") == 0 || operation.compare("/") == 0)
+    {
+        std::cout << std::fixed << std::setprecision(2) << "=" << numOne / numTwo << std::endl;
+        return true;
+    }
+    else if (operation.compare("end") == 0)
+    {
+        return false;
+    }
+    else
+    {
+        std::cout << "*Operation Not Recognized, Please Try Again!*" << std::endl;
+        return true;
+    }
+}
 
 int main()
 {
@@ -13,10 +59,12 @@ int main()
     double numTwo;
     std::string operation;
     double total;
+    bool whileLoopStopper = true;
 
 
     do
     {
+        printWelcome();
         std::cout << "Number1: ";
         numOne = validateDouble(numOne);
 
@@ -26,32 +74,12 @@ int main()
         std::cout << "Operation: ";
         operation = validateString(operation);
 
-        std::for_each(operation.begin(), operation.end(), [](char& c) {
-            c = ::tolower(c); });
+        std::for_each(operation.begin(), operation.end(), [](char& c) {c = ::tolower(c); });
 
-        if (operation.compare("add") == 0 || operation.compare("+") == 0)
-        {
-            std::cout << "=" << numOne + numTwo << std::endl;
-        }
-        else if (operation.compare("subtract") == 0 || operation.compare("-") == 0)
-        {
-            std::cout << std::fixed << std::setprecision(2) << "=" << numOne - numTwo << std::endl;
-        }
-        else if (operation.compare("multiply") == 0 || operation.compare("*") == 0)
-        {
-            std::cout << std::fixed << std::setprecision(2) << "=" << numOne * numTwo << std::endl;
-        }
-        else if (operation.compare("divide") == 0 || operation.compare("/") == 0)
-        {
-            std::cout << std::fixed << std::setprecision(2) << "=" << numOne / numTwo << std::endl;
-        }
-        else
-        {
-            std::cout << "*Operation Not Recognized, Please Try Again!*" << std::endl;
-        }
-
+        whileLoopStopper = calculateResult(numOne, numTwo, operation);
         std::cout << "\n\n";
 
-    } while (true);
+
+    } while (whileLoopStopper);
 
 }
